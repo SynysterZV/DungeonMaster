4ed447d65bfe5b3eca53ae823612d82b8992fd40
Jacob Fenton <jfenton@daytonphysicians.com>
2023-09-02 18:10:19 -0400

    Remove dist

--- dist/util/loader.js
+++
@@ -1,8 +0,0 @@
-import { globSync } from "glob";
-export function loader(path, fn) {
-    globSync(`dist/${path}/**/*.js`, { absolute: true }).forEach(file => {
-        if (process.platform === "win32")
-            file = `file://${file}`;
-        return import(file).then(fn).catch(e => console.log(e));
-    });
-}
